4f7f35108bde26400e0d6e421952309891046618
Krikorov <krikorov@yandex.ru>
2021-12-13 22:33:47 +0300

    Ver1.0

--- include/Parser.h
+++ include/Parser.h
@@ -2,25 +2,42 @@
 
 #include <Arduino.h>
 
-#define IS_COMAND(a)   ((a) == 'G' || (a) == 'M' || (a) == 'W' || (a) == 'S')
+#define IS_COMAND(a)   ((a) == 'G' || (a) == 'M' || (a) == 'W' || (a) == 'S')  
+#define IS_NUMERIC(a) ((a) >= '0' && (a) <= '9')         
 
 class Parser {
 
   public:
     uint8_t m_machine;
     uint8_t m_pin;
-    uint8_t value;
+    uint8_t m_value;
+    uint16_t m_weight;
 
     bool parse (char *p){
-      
-      if (*p == 'G'){
-        p++;
-        while (!IS_COMAND (*p)){
-          m_machine = m_machine * 10 + *p++ - '0';
+      uint8_t *code;
+      while (*p !='\0'){
+        if (IS_COMAND(*p)){
+          switch (*P){
+            case 'G':
+              code = &m_machine;
+              break;
+            case 'M':
+              code = &m_pin;
+              break;
+            case 'V':
+              code = &m_value;
+              break;
+            case 'W':
+              code = &m_weight;
+              break;
+          }
+          p++;
         }
-
-      }
-      else  return false;
+        else if (IS_NUMERIC(*p)){
+          *code = (*code ) * 10 + (*p) - '0';
+          p++;
+        }
+        else return false;
 
       return true;
     }
